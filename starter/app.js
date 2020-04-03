4db62d0a8b296d7c2df3a672819b6d20d3e130c7
Himynameislou <louie1598@gmail.com>
2020-04-03 21:59:50 +0000

    L80, moved around some code and created an initialization file

--- starter/app.js
+++ starter/app.js
@@ -54,28 +54,39 @@ var UIController = (function () {
 
 var controller = (function(budgetCtrl, UICtrl){
 
-  var DOM = UIController.getDOMstrings();
+  var setupEventListeners = function() {
+
+    var DOM = UIController.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+  // Using an event object for the keypress.  Notice we put it outside in the global area bc it is not tied to a single button.
+    document.addEventListener('keypress', function(event){
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+
+  }
+
 
   var ctrlAddItem = function() {
     // 1. Get the field input data
     var input = UIController.getInput();
-    console.log(input);
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4.Calculate the budget.
     // 5. Display the budget on the UI
+  };
+
+  return {
+    init: function() {
+      console.log('App has started');
+      setupEventListeners();
+    }
   }
 
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-// Using an event object for the keypress.  Notice we put it outside in the global area bc it is not tied to a single button.
-
-
-  document.addEventListener('keypress', function(event){
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
-    }
-  })
 
 
 })(budgetController,UIController);
+
+controller.init();
